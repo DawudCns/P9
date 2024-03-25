7f427b4c8b71bf6162e7a269d7330fdaac42d9ad
Dawud Cns <davidhc@hotmail.be>
2024-03-25 06:50:31 +0100

    optimisation perfo

--- purgecss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: [
-    "P9/ninacarducci.github.io-main/*.html",
-    "P9/ninacarducci.github.io-main/assets/bootstrap/*.js",
-  ], // Chemins vers les fichiers HTML et JS à analyser
-  css: ["P9/ninacarducci.github.io-main/assets/bootstrap/*.css"], // Chemins vers vos fichiers CSS à purger
-  // Autres options de configuration
-};
