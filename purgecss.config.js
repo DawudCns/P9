ec130798be08d5a75cdeb890d4efc7b672613ffa
Dawud Cns <davidhc@hotmail.be>
2024-03-25 04:54:31 +0100

    shema

---
+++ purgecss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    "P9/ninacarducci.github.io-main/*.html",
+    "P9/ninacarducci.github.io-main/assets/bootstrap/*.js",
+  ], // Chemins vers les fichiers HTML et JS à analyser
+  css: ["P9/ninacarducci.github.io-main/assets/bootstrap/*.css"], // Chemins vers vos fichiers CSS à purger
+  // Autres options de configuration
+};
